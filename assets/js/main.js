f6648d20622d6768c7f7f2d780cc17902e9c7781
Katerina <47293391+katerinka1200@users.noreply.github.com>
2020-02-22 18:28:49 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -249,4 +249,14 @@ FB.getLoginStatus(function(response) {
     }
 }
 
+<fb:login-button 
+  scope="public_profile,email"
+  onlogin="checkLoginState();">
+</fb:login-button>
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
 
